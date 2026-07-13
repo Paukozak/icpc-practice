f5a22963cae071e28c7cf4271fbebf6eae3444ed
Paula <paukozakr@gmail.com>
2026-07-13 10:37:05 -0300

    resolvi dos ejercicios

---
+++ c++/petyaAndStrings.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s1, s2;
+    char c1, c2;
+    int sal, i;
+    bool fin;
+
+    sal=0;
+
+    cin>>s1>>s2;
+
+    i=0;
+    fin=false;
+
+    while ((i<(s1.size())) && fin==false) {
+
+        c1=tolower(s1[i]);
+        c2=tolower(s2[i]);
+
+        if (c1!=c2) {
+            fin=true;
+            if (c1>c2) {
+                sal=1;
+            } else {
+                sal=-1;
+            }
+        }
+
+        i+=1;
+
+    }
+
+    cout<<sal;
+
+    return 0;
+}
