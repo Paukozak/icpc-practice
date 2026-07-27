39b23aca8363712a02da1ab8c9369c508126ad75
Matias Lima <matiaselima@hotmail.com>
2026-07-27 13:13:20 -0300

    mejorando multiple_downloads (sigue sin andar)

--- tap-2023/multiple_downloads.cpp
+++ tap-2023/multiple_downloads.cpp
@@ -7,18 +7,19 @@ int main () {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int cant, velocidad, tamaño, pos;
+    int cant, velocidad, tamaño;
     cin>>cant>>velocidad;
 
 
 
-    char prio;
-    string lista;
+    char prioridad;
+    bool prio,unprio;
     vector<double> tamaño_prio, tamaño_unprio;
+    vector<int> pos_prio, pos_unprio;
 
     for (int i=0; i<cant; i++) {
-        cin>>prio>>tamaño;
-        if (prio==*"P"){
+        cin>>prioridad>>tamaño;
+        if (prioridad==*"P"){
             tamaño_prio.push_back(tamaño);
         } else {
             tamaño_unprio.push_back(tamaño);
@@ -28,7 +29,7 @@ int main () {
     double velocidad_prio, velocidad_unprio, tiempo, tiempo_min,tiempo_total;
     
     tiempo_total=0;
-    for (int i=0; i<cant; i++){
+    while (not tamaño_prio.empty() || not tamaño_unprio.empty()){
         tiempo_min=513;
         if (tamaño_prio.empty()){
             velocidad_unprio=velocidad;
@@ -42,18 +43,26 @@ int main () {
         for (size_t j=0; j<tamaño_prio.size();j++){
             tiempo=tamaño_prio[j]/(velocidad_prio/tamaño_prio.size());
             if (tiempo<tiempo_min){
+                pos_prio.clear();
+                pos_unprio.clear();
                 tiempo_min=tiempo;
-                lista="P";
-                pos=j;
+                prio=true;
+                pos_prio.push_back(j);
+            } else if (tiempo==tiempo_min){
+                pos_prio.push_back(j);
             }
             //cout<< "calculo el tiempo menor de las priorizadas"<<"\n";
         }
         for (size_t j=0; j<tamaño_unprio.size();j++){
             tiempo=tamaño_unprio[j]/(velocidad_unprio/tamaño_unprio.size());
             if (tiempo<tiempo_min){
+                pos_unprio.clear();
+                pos_prio.clear();
                 tiempo_min=tiempo;
-                lista="N";
-                pos=j;
+                unprio=true;
+                pos_unprio.push_back(j);
+            } else if (tiempo==tiempo_min){
+                pos_unprio.push_back(j);
             }
             //cout <<tamaño_unprio[j];
             //cout << "calculo el tiempo menor de los no priorizadas"<<"\n";
@@ -70,13 +79,28 @@ int main () {
             //cout <<"recalculo una no priorizada"<<"\n";
         }
 
-        if (lista=="N"){
-            tamaño_unprio.erase(tamaño_unprio.begin()+pos);
+        if (prio && unprio){
+            for (size_t j=0; j<pos_prio.size();j++){
+                tamaño_prio.erase(tamaño_prio.begin()+pos_prio[j]);
+            }
+            for (size_t j=0; j<pos_unprio.size();j++){
+                tamaño_unprio.erase(tamaño_unprio.begin()+pos_unprio[j]);
+            }
+        }
+        else if (unprio) {
+            for (size_t j=0; j<pos_unprio.size();j++){
+                tamaño_unprio.erase(tamaño_unprio.begin()+pos_unprio[j]);
+            }
             //cout<<"borro una no priorizada"<<"\n";
         } else {
-            tamaño_prio.erase(tamaño_prio.begin()+pos);
+            for (size_t j=0; j<pos_prio.size();j++){
+                tamaño_prio.erase(tamaño_prio.begin()+pos_prio[j]);
+            }
             //cout <<"borro una priorizada"<<"\n";
         }
+
+        pos_prio.clear();
+        pos_unprio.clear();
 
         
 
